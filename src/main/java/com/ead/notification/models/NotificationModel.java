9a15b8050083ffb2d325264b3c76791c3569c428
jehmes <jeihmesthales@hotmail.com>
2023-04-22 15:20:46 -0300

    Configured microservice

---
+++ src/main/java/com/ead/notification/models/NotificationModel.java
@@ -0,0 +1,60 @@
+package com.ead.notification.models;
+
+import com.ead.notification.enums.NotificationStatus;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import org.hibernate.annotations.CreationTimestamp;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@Entity
+@Table(name = "TB_NOTIFICATIONS")
+public class NotificationModel implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID notificationId;
+
+    @Column(nullable = false)
+    private UUID userId;
+    @Column(nullable = false, length = 150)
+    private String title;
+    @Column(nullable = false)
+    private String message;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'")
+    @Column(nullable = false)
+    @CreationTimestamp
+    private LocalDateTime creationDate;
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private NotificationStatus userStatus;
+
+    public UUID getNotificationId() {
+        return notificationId;
+    }
+
+    public UUID getUserId() {
+        return userId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public LocalDateTime getCreationDate() {
+        return creationDate;
+    }
+
+    public NotificationStatus getUserStatus() {
+        return userStatus;
+    }
+}
