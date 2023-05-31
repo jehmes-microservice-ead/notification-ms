18c967ffafbeca9d439d5a29195eea2ef42f1d87
jehmes <jeihmesthales@hotmail.com>
2023-05-30 21:31:09 -0300

    fixing deprecated methods

--- src/main/java/com/ead/notification/configs/security/JwtProvider.java
+++ src/main/java/com/ead/notification/configs/security/JwtProvider.java
@@ -1,11 +1,14 @@
 package com.ead.notification.configs.security;
 
 import io.jsonwebtoken.*;
+import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+
+import java.nio.charset.StandardCharsets;
 
 @Component
 public class JwtProvider {
@@ -15,18 +18,25 @@ public class JwtProvider {
     private String jwtSecret;
 
     public String getSubjectJwt(String token) {
-        return Jwts.parser().setSigningKey(jwtSecret).parseClaimsJws(token).getBody().getSubject();
+        return Jwts.parserBuilder()
+                .setSigningKey(Keys.hmacShaKeyFor(jwtSecret.getBytes(StandardCharsets.UTF_8))).build()
+                .parseClaimsJws(token).getBody().getSubject();
     }
 
     public String getClaimNameJwt(String token, String claimName) {
-        return Jwts.parser().setSigningKey(jwtSecret).parseClaimsJws(token).getBody().get(claimName).toString();
+        return Jwts.parserBuilder()
+                .setSigningKey(Keys.hmacShaKeyFor(jwtSecret.getBytes(StandardCharsets.UTF_8))).build()
+                .parseClaimsJws(token).getBody()
+                .get(claimName).toString();
     }
 
     public boolean validateJwt(String authToken) {
         try {
-            Jwts.parser().setSigningKey(jwtSecret).parseClaimsJws(authToken);
+            Jwts.parserBuilder()
+                    .setSigningKey(Keys.hmacShaKeyFor(jwtSecret.getBytes(StandardCharsets.UTF_8))).build()
+                    .parseClaimsJws(authToken);
             return true;
-        } catch (SignatureException e) {
+        } catch (SecurityException e) {
             log.error("Invalid JWT signature: {}", e.getMessage());
         } catch (MalformedJwtException e) {
             log.error("Invalid JWT token: {}", e.getMessage());
